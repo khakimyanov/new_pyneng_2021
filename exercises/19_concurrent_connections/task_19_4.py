3839897fc5d78065cacf32dbf33b4f30ae2c48fa
khakimyanov <asamat@mail.ru>
2021-03-14 06:27:21 -0500

    Done all

--- exercises/19_concurrent_connections/task_19_4.py
+++ exercises/19_concurrent_connections/task_19_4.py
@@ -106,100 +106,12 @@ R3#
 Для выполнения задания можно создавать любые дополнительные функции.
 """
 # -*- coding: utf-8 -*-
-
-"""
-Раньше это задание звучало так:
-Задание 20.4
-
-Создать функцию send_commands_to_devices, которая отправляет команду show или config на разные устройства в параллельных потоках, а затем записывает вывод команд в файл.
-
-Параметры функции:
-* devices - список словарей с параметрами подключения к устройствам
-* show - команда show, которую нужно отправить (по умолчанию, значение None)
-* config - команды конфигурационного режима, которые нужно отправить (по умолчанию, значение None)
-* filename - имя файла, в который будут записаны выводы всех команд
-* limit - максимальное количество параллельных потоков (по умолчанию 3)
-
-Функция ничего не возвращает.
-
-Вывод команд должен быть записан в файл в таком формате (перед выводом команды надо написать имя хоста и саму команду):
-
-R1#sh ip int br
-Interface                  IP-Address      OK? Method Status                Protocol
-Ethernet0/0                192.168.100.1   YES NVRAM  up                    up
-Ethernet0/1                192.168.200.1   YES NVRAM  up                    up
-R2#sh arp
-Protocol  Address          Age (min)  Hardware Addr   Type   Interface
-Internet  192.168.100.1          76   aabb.cc00.6500  ARPA   Ethernet0/0
-Internet  192.168.100.2           -   aabb.cc00.6600  ARPA   Ethernet0/0
-Internet  192.168.100.3         173   aabb.cc00.6700  ARPA   Ethernet0/0
-R3#sh ip int br
-Interface                  IP-Address      OK? Method Status                Protocol
-Ethernet0/0                192.168.100.3   YES NVRAM  up                    up
-Ethernet0/1                unassigned      YES NVRAM  administratively down down
-
-Пример вызова функции:
-In [5]: send_commands_to_devices(devices, show='sh clock', filename='result.txt')
-
-In [6]: cat result.txt
-R1#sh clock
-*04:56:34.668 UTC Sat Mar 23 2019
-R2#sh clock
-*04:56:34.687 UTC Sat Mar 23 2019
-R3#sh clock
-*04:56:40.354 UTC Sat Mar 23 2019
-
-In [11]: send_commands_to_devices(devices, config='logging 10.5.5.5', filename='result.txt')
-
-In [12]: cat result.txt
-config term
-Enter configuration commands, one per line.  End with CNTL/Z.
-R1(config)#logging 10.5.5.5
-R1(config)#end
-R1#
-config term
-Enter configuration commands, one per line.  End with CNTL/Z.
-R2(config)#logging 10.5.5.5
-R2(config)#end
-R2#
-config term
-Enter configuration commands, one per line.  End with CNTL/Z.
-R3(config)#logging 10.5.5.5
-R3(config)#end
-R3#
-
-In [13]: send_commands_to_devices(devices,
-                                  config=['router ospf 55', 'network 0.0.0.0 255.255.255.255 area 0'],
-                                  filename='result.txt')
-
-In [14]: cat result.txt
-config term
-Enter configuration commands, one per line.  End with CNTL/Z.
-R1(config)#router ospf 55
-R1(config-router)#network 0.0.0.0 255.255.255.255 area 0
-R1(config-router)#end
-R1#
-config term
-Enter configuration commands, one per line.  End with CNTL/Z.
-R2(config)#router ospf 55
-R2(config-router)#network 0.0.0.0 255.255.255.255 area 0
-R2(config-router)#end
-R2#
-config term
-Enter configuration commands, one per line.  End with CNTL/Z.
-R3(config)#router ospf 55
-R3(config-router)#network 0.0.0.0 255.255.255.255 area 0
-R3(config-router)#end
-R3#
-
-
-Для выполнения задания можно создавать любые дополнительные функции.
-"""
 import logging
 import yaml
 from netmiko import ConnectHandler
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 
@@ -211,22 +123,21 @@ def send_config_command_set(device, command_set):
     start_msg = "===> {} Connection: {}"
     received_msg = "<=== {} Received:   {}"
     ip = device['host']
-    output = ''
     logging.info(start_msg.format(datetime.now().time(), ip))
 
     with ConnectHandler(**device) as ssh:
         ssh.enable()
-        prompt = ssh.find_prompt()
         result = ssh.send_config_set(command_set, strip_command=False)
         logging.info(received_msg.format(datetime.now().time(), ip))
     return result
+    #return f"{result}\n"
 
 def send_show_command(device, show):
     start_msg = "===> {} Connection: {}"
     received_msg = "<=== {} Received:   {}"
     ip = device['host']
     logging.info(start_msg.format(datetime.now().time(), ip))
-    
+
     with ConnectHandler(**device) as ssh:
         ssh.enable()
         prompt = ssh.find_prompt()
@@ -234,9 +145,19 @@ def send_show_command(device, show):
         logging.info(received_msg.format(datetime.now().time(), ip))
 
     return prompt + result + '\n'
+    #Как всегда Наталья это делает более элегантно
+    #return f"{prompt}{command}\n{result}\n"
 
-def send_commands_to_devices(device, show=None, config=None, filename=None, limit=3):
+def send_commands_to_devices(devices, filename, *, show=None, config=None, limit=3):
+    if show and config:
+        raise ValueError('Function takes only 2 positional argument, 3 were given')
     with ThreadPoolExecutor(max_workers=limit) as executor:
+        #Вместо такого моего if у Натальи
+        #command = show if show else config
+        #function = send_show_command if show else send_config_command_set
+        #и тогда запуск
+        #futures = [executor.submit(function, device, command) for device in devices]
+
         if show:
             f_result = [executor.submit(send_show_command, device, show) for device in devices]
         elif config:
@@ -249,9 +170,9 @@ def send_commands_to_devices(device, show=None, config=None, filename=None, limi
 if __name__ == "__main__":
     with open('devices.yaml') as f:
         devices = yaml.safe_load(f)
-    
-    #send_commands_to_devices(devices, show='sh clock', filename='result.txt')
-    #send_commands_to_devices(devices, config='logging 10.5.5.5', filename='result.txt')
-    send_commands_to_devices(devices,
-                                  config=['router ospf 55', 'network 0.0.0.0 255.255.255.255 area 0'],
-                                  filename='result.txt')
+    commands = ['router ospf 55', 'network 0.0.0.0 255.255.255.255 area 0']
+
+    #send_commands_to_devices(devices, 'result.txt', show='sh clock', config='logging 1.1.1.1')
+    #send_commands_to_devices(devices, 'result.txt', show='sh clock')
+    #send_commands_to_devices(devices, 'result.txt', config='logging 10.5.5.5')
+    send_commands_to_devices(devices, 'result.txt', config=commands)
